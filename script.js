b76ef6b4c1caedd4c708a15119eb3142070201bc
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-12-02 16:53:04 +0500

    i build to ti

---
+++ script.js
@@ -0,0 +1,41 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.getElementById("reset-btn");
+let turn = true;
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("You win");
+      }
+    }
+  }
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => 
+    if (turn) {
+      box.innerText = "X";
+      turn = false;
+    } else {
+      box.innerText = "0";
+      turn = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
